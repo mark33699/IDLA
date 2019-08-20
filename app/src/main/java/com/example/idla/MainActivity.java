30b99221d562664b6d4ed5a5bd930cbf6115d92c
mark.HsieH <mark.h@sunndev.com>
2019-08-20 15:53:45 +0800

    init

---
+++ app/src/main/java/com/example/idla/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.idla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MarkFly","Hello World");
+    }
+}
