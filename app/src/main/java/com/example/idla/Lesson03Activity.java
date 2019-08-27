c170034303667db49446380cb267e654b8a50d31
mark.HsieH <mark.h@sunndev.com>
2019-08-27 18:32:19 +0800

    lesson 00 簡單快速弄個首頁

---
+++ app/src/main/java/com/example/idla/Lesson03Activity.java
@@ -0,0 +1,14 @@
+package com.example.idla;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Lesson03Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson03);
+    }
+}
--- app/src/main/java/com/example/idla/MainActivity.java
+++ app/src/main/java/com/example/idla/MainActivity.java
@@ -1,22 +1,60 @@
 package com.example.idla;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView; //畫面
+    private List<String> items; //資料
+    private ArrayAdapter arrayAdapter; //適配器
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.d("MarkFly","Hello World");
-        if (BuildConfig.foo)
-        {
-            TextView textView = findViewById(R.id.main_label);
-            textView.setText("我是傻子");
-        }
+
+        listView = findViewById(R.id.main_listView); //取得畫面
+
+        setupData(); //產生資料
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,items); //使用樣式跟資料產生適配器
+        listView.setAdapter(arrayAdapter); //餵給listView
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //監聽事件(大括號裡除了實作方法, 不能寫別的東西)
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) { //變數名稱不明
+
+                switch (i) //不會整組長出來
+                {
+                    case 0:
+                    {
+                        Intent intent = new Intent(MainActivity.this,Lesson03Activity.class);//不能只寫.this
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void setupData() {
+        items = new ArrayList<>();
+        items.add("Lesson 03 - Activity");
+        items.add("Lesson 04 - Layout");
+        items.add("Lesson 05 - 必用元件");
+        items.add("Lesson 06 - 常用元件");
     }
 }
