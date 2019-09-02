797abb7687036e4a30deea95432cc4f217f2103e
mark.HsieH <mark.h@sunndev.com>
2019-09-02 18:09:34 +0800

    lesson 04 constrainting

---
+++ app/src/main/java/com/example/idla/Lesson05/Lesson05Activity.java
@@ -0,0 +1,16 @@
+package com.example.idla.Lesson05;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.idla.R;
+
+public class Lesson05Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson05);
+    }
+}
