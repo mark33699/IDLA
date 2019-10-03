2a5592629e9b398070538c89644228018b6fa321
mark <mark.appledeveloper@gmail.com>
2019-10-04 01:43:10 +0800

    lesson 12  call API (POST)

--- app/src/main/java/com/example/idla/Lesson13/Lesson13Activity.java
+++ app/src/main/java/com/example/idla/Lesson13/Lesson13Activity.java
@@ -1,8 +1,13 @@
 package com.example.idla.Lesson13;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,25 +19,42 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class Lesson13Activity extends AppCompatActivity
 {
-
-
+    EditText editTextEmail;
+    EditText editTextPassword;
+    TextView textViewResult;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson13);
+
+        editTextEmail = findViewById(R.id.editText5);
+        editTextPassword = findViewById(R.id.editText10);
+        textViewResult = findViewById(R.id.textView12);
+        progressBar = findViewById(R.id.progressBar3);
     }
 
     public void ok(View view)
     {
-        call("https://reqres.in/api/users/2");
+        View viewFocus = this.getCurrentFocus();
+        if (viewFocus != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(viewFocus.getWindowToken(), 0);
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        callPOST("https://reqres.in/api/register");
+
+//        callGET("https://reqres.in/api/users/2");
 
         //官方的範例會閃退= =
 //        String result = "";
@@ -46,7 +68,49 @@ public class Lesson13Activity extends AppCompatActivity
 
     OkHttpClient client = new OkHttpClient();
 
-    void call(String url)
+    void callPOST(String url)
+    {
+        FormBody body = new FormBody.Builder()
+                .add("email", String.valueOf(editTextEmail.getText()))
+                .add("password", String.valueOf(editTextPassword.getText()))
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        Call call = client.newCall(request);
+
+        call.enqueue(new Callback()
+        {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException
+            {
+                final String result = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textViewResult.setText(result);
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e)
+            {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textViewResult.setText("failure");
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        });
+    }
+
+    void callGET(String url)
     {
         Request request = new Request.Builder()
                 .url(url)
@@ -70,8 +134,6 @@ public class Lesson13Activity extends AppCompatActivity
             }
         });
     }
-
-
 
     String run(String url) throws IOException
     {
