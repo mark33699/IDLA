d46cd6de6f9d2817d51c1831f42e217aefa71816
mark <mark.appledeveloper@gmail.com>
2019-10-13 00:58:15 +0800

    lesson 21 搞到會一直閃退

--- app/src/main/java/com/example/idla/IDLAWidget.java
+++ app/src/main/java/com/example/idla/IDLAWidget.java
@@ -1,9 +1,11 @@
 package com.example.idla;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.widget.Button;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -11,41 +13,80 @@ import android.widget.RemoteViews;
  */
 public class IDLAWidget extends AppWidgetProvider
 {
+    AppWidgetManager currentAppWidgetManager;
+    static RemoteViews remoteView;
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(com.example.idla.R.string.appwidget_text);
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId)
+    {
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), com.example.idla.R.layout.idlawidget);
-//        views.setTextViewText(com.example.idla.R.id.appwidget_text, widgetText);
+        remoteView = new RemoteViews(context.getPackageName(), com.example.idla.R.layout.idlawidget);
 
+//        CharSequence widgetText = context.getString(com.example.idla.R.string.appwidget_text);
+//        remoteView.setTextViewText(com.example.idla.R.id.appwidget_text, widgetText);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,0,new Intent().setAction("" + appWidgetId),0);
+        remoteView.setOnClickPendingIntent(R.id.appwidget_btn,pendingIntent);
 
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.updateAppWidget(appWidgetId, remoteView);
     }
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+        currentAppWidgetManager = appWidgetManager;
+        Log.d("MF❤️","APP Widget onEnabled");
+
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
+        for (int appWidgetId : appWidgetIds)
+        {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
     @Override
-    public void onEnabled(Context context) {
+    public void onEnabled(Context context)
+    {
         // Enter relevant functionality for when the first widget is created
+        Log.d("MF❤️","APP Widget onEnabled");
     }
 
     @Override
-    public void onDisabled(Context context) {
+    public void onDisabled(Context context)
+    {
         // Enter relevant functionality for when the last widget is disabled
+        Log.d("MF❤️","APP Widget onDisabled");
     }
 
-    public void pressMe(Button button)
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds)
     {
-        button.setText("按了按了按了按了按了按了按了按了按了按了");
+        super.onDeleted(context, appWidgetIds);
+        Log.d("MF❤️","APP Widget onDeleted");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        super.onReceive(context, intent);
+        Log.d("MF❤️","APP Widget onReceive");
+
+        if (remoteView == null)
+        {
+            return;
+        }
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text_new);
+        remoteView.setTextViewText(R.id.appwidget_btn, widgetText);
+
+        currentAppWidgetManager.updateAppWidget(Integer.parseInt(intent.getAction()),remoteView);
+    }
+
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds)
+    {
+        super.onRestored(context, oldWidgetIds, newWidgetIds);
+        Log.d("MF❤️","APP Widget onRestored");
     }
 }
 
