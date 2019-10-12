f91ee6b40c047933afc3d12181100bcc1a6c75b5
mark <mark.appledeveloper@gmail.com>
2019-10-12 21:42:33 +0800

    lesson 21 研究一下怎麼按button

--- app/src/main/java/com/example/idla/IDLAWidget.java
+++ app/src/main/java/com/example/idla/IDLAWidget.java
@@ -3,12 +3,14 @@ package com.example.idla;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.widget.Button;
 import android.widget.RemoteViews;
 
 /**
  * Implementation of App Widget functionality.
  */
-public class IDLAWidget extends AppWidgetProvider {
+public class IDLAWidget extends AppWidgetProvider
+{
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -16,7 +18,8 @@ public class IDLAWidget extends AppWidgetProvider {
         CharSequence widgetText = context.getString(com.example.idla.R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), com.example.idla.R.layout.idlawidget);
-        views.setTextViewText(com.example.idla.R.id.appwidget_text, widgetText);
+//        views.setTextViewText(com.example.idla.R.id.appwidget_text, widgetText);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -38,6 +41,11 @@ public class IDLAWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public void pressMe(Button button)
+    {
+        button.setText("按了按了按了按了按了按了按了按了按了按了");
     }
 }
 
