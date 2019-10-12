e2b50ea6ab6d69dd80c287cf879e7789986514f8
mark <mark.appledeveloper@gmail.com>
2019-10-13 01:00:15 +0800

    lesson 21 總算可以按按鈕了....

--- app/src/main/java/com/example/idla/IDLAWidget.java
+++ app/src/main/java/com/example/idla/IDLAWidget.java
@@ -1,9 +1,11 @@
 package com.example.idla;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.widget.Button;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -15,19 +17,19 @@ public class IDLAWidget extends AppWidgetProvider
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(com.example.idla.R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), com.example.idla.R.layout.idlawidget);
-//        views.setTextViewText(com.example.idla.R.id.appwidget_text, widgetText);
-
+        RemoteViews remoteView = new RemoteViews(context.getPackageName(), com.example.idla.R.layout.idlawidget);
+        remoteView.setOnClickPendingIntent(com.example.idla.R.id.appwidget_btn, PendingIntent.getActivity(context,0,new Intent(context,MainActivity.class),0));
 
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.updateAppWidget(appWidgetId, remoteView);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
+        Log.d("MF❤️","APP Widget onUpdate");
+
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
@@ -36,6 +38,8 @@ public class IDLAWidget extends AppWidgetProvider
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+        Log.d("MF❤️","APP Widget onEnabled");
+
     }
 
     @Override
@@ -43,9 +47,10 @@ public class IDLAWidget extends AppWidgetProvider
         // Enter relevant functionality for when the last widget is disabled
     }
 
-    public void pressMe(Button button)
-    {
-        button.setText("按了按了按了按了按了按了按了按了按了按了");
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.d("MF❤️","APP Widget onReceive");
     }
 }
 
